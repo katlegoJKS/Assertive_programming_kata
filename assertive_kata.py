5e92d8d3c91e3fe9d52c95916317da90325b6fd6
katlegoJKS <katlego.malatjie@umuzi.org>
2020-07-29 08:55:31 +0200

    Updated main script

--- assertive_kata.py
+++ assertive_kata.py
@@ -1,18 +1,21 @@
-MORSE_CODE_DICT = {'A':'.-', 'B':'-...', 
-		           'C':'-.-.', 'D':'-..', 'E':'.', 
-		           'F':'..-.', 'G':'--.', 'H':'....', 
-		           'I':'..', 'J':'.---', 'K':'-.-', 
-		           'L':'.-..', 'M':'--', 'N':'-.', 
-		           'O':'---', 'P':'.--.', 'Q':'--.-', 
-		           'R':'.-.', 'S':'...', 'T':'-', 
-		           'U':'..-', 'V':'...-', 'W':'.--', 
-		           'X':'-..-', 'Y':'-.--', 'Z':'--..', 
-		           '1':'.----', '2':'..---', '3':'...--', 
-		           '4':'....-', '5':'.....', '6':'-....', 
-		           '7':'--...', '8':'---..', '9':'----.', 
-		           '0':'-----', ', ':'--..--', '.':'.-.-.-', 
-		           '?':'..--..', '/':'-..-.', '-':'-....-', 
-		           '(':'-.--.', ')':'-.--.-'}
+MORSE_CODE_DICT = {
+    'A':'.-', 'B':'-...', 'C':'-.-.',
+    'D':'-..', 'E':'.', 'F':'..-.',
+    'G':'--.', 'H':'....', 'I':'..',
+    'J':'.---', 'K':'-.-', 'L':'.-..',
+    'M':'--', 'N':'-.', 'O':'---',
+    'P':'.--.', 'Q':'--.-', 'R':'.-.',
+    'S':'...', 'T':'-', 'U':'..-',
+    'V':'...-', 'W':'.--', 'X':'-..-',
+    'Y':'-.--', 'Z':'--..',
+    '1':'.----', '2':'..---', '3':'...--',
+    '4':'....-', '5':'.....', '6':'-....',
+    '7':'--...', '8':'---..', '9':'----.',
+    '0':'-----',
+    ', ':'--..--', '.':'.-.-.-', '?':'..--..',
+    '/':'-..-.', '-':'-....-',  '(':'-.--.',
+    ')':'-.--.-'
+    }
 
 def encrypt_to_morse_code(text):
     morse_code = ' '
@@ -38,24 +41,21 @@ def decrypt_to_text(text):
             else:
                 decrypt += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT.values()).index(encrypted_text)]
                 encrypted_text = ''
-
     return decrypt
     
-def main():
+
+if __name__ == "__main__":
     text = "Hi there"
     cipher = encrypt_to_morse_code(text.upper())
     assert len(text) != 0, 'You cannot encrypt a blank text'
-    print("Original message:",text)
-    print("Message in Morse Code:",cipher)
-    print("Message length:",len(text))
+    print("Original message: ",text)
+    print("Message in Morse Code: ",cipher)
+    print("Message length: ",len(text))
   
-    text = ".... ..  - .... . .-. ."
-    text_length =list(text.split(' '))
-    cipher = decrypt_to_text(text)
-    assert len(text) != 0, 'You cannot decrypt a blank text'
-    print("\nMorse Code:",text)
-    print("Message in plain text:",cipher)
+    morse_code = ".... ..  - .... . .-. ."
+    text_length =list(morse_code.split(' '))
+    cipher = decrypt_to_text(morse_code)
+    assert len(morse_code) != 0, 'You cannot decrypt a blank text'
+    print("\nMorse Code:", morse_code)
+    print("Message in plain text:", cipher)
     print("Message length: {}".format(len(text_length)))
-
-if __name__ == "__main__":
-    main()
